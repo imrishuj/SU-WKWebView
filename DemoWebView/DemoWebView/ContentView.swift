afe384c4037e3c15c18f9ea6860685b43863c4d5
imrishuj <65715489+imrishuj@users.noreply.github.com>
2020-06-18 21:23:55 +0530

    WKWebView in SwiftUi

---
+++ DemoWebView/DemoWebView/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  DemoWebView
+//
+//  Created by Juspay on 18/06/20.
+//  Copyright © 2020 Rishabh. All rights reserved.
+//
+
+import SwiftUI
+import WebKit
+
+struct ContentView: View {
+    
+    @State private var urlString = ""
+    @State private var shouldHide = false
+    
+    var body: some View {
+        
+        VStack {
+            
+            CustomWKWebView(urlString: self.$urlString)
+            
+            if !shouldHide {
+
+                Button(action: {
+                    self.urlString = "https://www.apple.com"
+                    self.shouldHide = true
+                }) {
+                    Text("Open WebPage")
+                }
+            }
+        }
+    }
+}
+
+struct CustomWKWebView: UIViewRepresentable {
+    
+    @Binding var urlString: String
+        
+    func makeUIView(context: Context) -> WKWebView  {
+        let webView = WKWebView()
+        return webView
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        
+        if let url = URL(string: self.urlString) {
+            uiView.load(URLRequest(url: url))
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
